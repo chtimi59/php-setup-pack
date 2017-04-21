56c7c065b3654809a23247f40edc6f162f435fb0
jan <jdorgeville@gmail.com>
2017-04-21 04:35:25 -0400

    remove ../conf file also

--- removeAccess.php
+++ removeAccess.php
@@ -3,9 +3,16 @@ $confPath = urldecode($_GET['conf']);
 @unlink("$confPath.hash");
 @unlink("index.php");
 @unlink("generate.php");
-@unlink("setup.sql");
 @rename("tmp.htaccess", ".htaccess");
 @unlink("WARNING.txt");
 @unlink("removeAccess.php");
+
+if (file_exists ("setup.conf")) @unlink("setup.conf");
+if (file_exists ("setup.sql"))  @unlink("setup.sql");
+if (file_exists ("users.sql"))  @unlink("users.sql");
+if (file_exists ("../setup.conf")) @unlink("../setup.conf");
+if (file_exists ("../setup.sql"))  @unlink("../setup.sql");
+if (file_exists ("../users.sql"))  @unlink("../users.sql");
+
 echo("done!");
 ?>
