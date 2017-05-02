aa48e8bc8c6a0dbc917951c72cdfc9e9409fd590
jdorgeville <jdorgeville@spiria.com>
2017-05-02 14:13:09 -0400

    cosmetic

--- generate.php
+++ generate.php
@@ -35,128 +35,129 @@ function printKO($str) { echo "<tr><td>$str</td><td class='error'>KO</td>\n"; }
 echo "<table>\n";
 $DATA = array();
 if(true){try{do
-{   
-    if (!isset ($_POST['conf_path'])) { printKO("conf_path missing!"); break; } 
-    if (empty($_POST['conf_path'])) { printKO("conf_path empty!"); break; } 
+{
+    if (!isset ($_POST['conf_path'])) { printKO("conf_path missing!"); break; }
+    if (empty($_POST['conf_path'])) { printKO("conf_path empty!"); break; }
     $confPath = $_POST['conf_path'];
     $hashPath = "$confPath.hash";
-    if (!isset ($_POST['conf_hash'])) { printKO("conf_hash missing!"); break; } 
-    if (empty($_POST['conf_hash'])) { printKO("conf_hash empty!"); break; } 
+    if (!isset ($_POST['conf_hash'])) { printKO("conf_hash missing!"); break; }
+    if (empty($_POST['conf_hash'])) { printKO("conf_hash empty!"); break; }
     $secretHash = $_POST['conf_hash'];
 
-    if (!isset ($_POST['base_url'])) { printKO("base_url missing!"); break; } 
-    if (empty($_POST['base_url'])) { printKO("base_url empty!"); break; } 
+    if (!isset ($_POST['base_url'])) { printKO("base_url missing!"); break; }
+    if (empty($_POST['base_url'])) { printKO("base_url empty!"); break; }
     $DATA['base_url'] = $_POST['base_url'];
-    
+
     if ($setup['features']['db']) {
-        if (!isset ($_POST['sql_host'])) { printKO("sql_host missing!"); break; } 
-        if (empty($_POST['sql_host'])) { printKO("sql_host empty!"); break; } 
+        if (!isset ($_POST['sql_host'])) { printKO("sql_host missing!"); break; }
+        if (empty($_POST['sql_host'])) { printKO("sql_host empty!"); break; }
         $DATA['sql_host'] = $_POST['sql_host'];
-        if (!isset ($_POST['sql_login'])) { printKO("sql_login missing!"); break; } 
-        if (empty($_POST['sql_login'])) { printKO("sql_login empty!"); break; } 
+        if (!isset ($_POST['sql_login'])) { printKO("sql_login missing!"); break; }
+        if (empty($_POST['sql_login'])) { printKO("sql_login empty!"); break; }
         $DATA['sql_login'] = $_POST['sql_login'];
-        if (!isset ($_POST['sql_pw'])) { printKO("sql_pw missing!"); break; } 
+        if (!isset ($_POST['sql_pw'])) { printKO("sql_pw missing!"); break; }
         $DATA['sql_pw'] = $_POST['sql_pw'];
         $DATA['sql_isPW'] = empty($_POST['sql_pw'])?false:true;
         if (!isset ($_POST['sql_db'])) { printKO("sql_db missing!"); break; }
-        if (empty($_POST['sql_db'])) { printKO("sql_db empty!"); break; }  
+        if (empty($_POST['sql_db'])) { printKO("sql_db empty!"); break; }
         $DATA['sql_db'] = $_POST['sql_db'];
     }
-    
+
     if ($setup['features']['user']) {
-        if (!isset ($_POST['user_table'])) { printKO("user_table missing!"); break; } 
-        if (empty($_POST['user_table'])) { printKO("user_table empty!"); break; } 
+        if (!isset ($_POST['user_table'])) { printKO("user_table missing!"); break; }
+        if (empty($_POST['user_table'])) { printKO("user_table empty!"); break; }
         $DATA['user_table'] = $_POST['user_table'];
     }
-    
+
     if ($setup['features']['admin']) {
-        if (!isset ($_POST['admin_email'])) { printKO("admin_email missing!"); break; } 
-        if (empty($_POST['admin_email'])) { printKO("admin_email empty!"); break; }  
+        if (!isset ($_POST['admin_email'])) { printKO("admin_email missing!"); break; }
+        if (empty($_POST['admin_email'])) { printKO("admin_email empty!"); break; }
         $DATA['admin_email'] = $_POST['admin_email'];
-        if (!isset ($_POST['admin_uuid'])) { printKO("admin_uuid missing!"); break; } 
-        if (empty($_POST['admin_uuid'])) { printKO("admin_uuid empty!"); break; }  
+        if (!isset ($_POST['admin_uuid'])) { printKO("admin_uuid missing!"); break; }
+        if (empty($_POST['admin_uuid'])) { printKO("admin_uuid empty!"); break; }
         $DATA['admin_uuid'] = $_POST['admin_uuid'];
-        if (!isset ($_POST['admin_pw'])) { printKO("admin_pw missing!"); break; } 
-        if (empty($_POST['admin_pw'])) { printKO("admin_pw empty!"); break; }  
+        if (!isset ($_POST['admin_pw'])) { printKO("admin_pw missing!"); break; }
+        if (empty($_POST['admin_pw'])) { printKO("admin_pw empty!"); break; }
         $DATA['admin_pw'] = $_POST['admin_pw'];
     }
-    
+
     if ($setup['features']['mail']) {
-        if (!isset ($_POST['smtp_host'])) { printKO("smtp_host missing!"); break; } 
-        if (empty($_POST['smtp_host'])) { printKO("smtp_host empty!"); break; }  
+        if (!isset ($_POST['smtp_host'])) { printKO("smtp_host missing!"); break; }
+        if (empty($_POST['smtp_host'])) { printKO("smtp_host empty!"); break; }
         $DATA['smtp_host'] = $_POST['smtp_host'];
-        if (!isset ($_POST['smtp_port'])) { printKO("smtp_port missing!"); break; } 
-        if (empty($_POST['smtp_port'])) { printKO("smtp_port empty!"); break; }  
+        if (!isset ($_POST['smtp_port'])) { printKO("smtp_port missing!"); break; }
+        if (empty($_POST['smtp_port'])) { printKO("smtp_port empty!"); break; }
         $DATA['smtp_port'] = $_POST['smtp_port'];
-        if (!isset ($_POST['smtp_login'])) { printKO("smtp_login missing!"); break; } 
-        if (empty($_POST['smtp_login'])) { printKO("smtp_login empty!"); break; }  
+        if (!isset ($_POST['smtp_login'])) { printKO("smtp_login missing!"); break; }
+        if (empty($_POST['smtp_login'])) { printKO("smtp_login empty!"); break; }
         $DATA['smtp_login'] = $_POST['smtp_login'];
-        if (!isset ($_POST['smtp_pw'])) { printKO("smtp_pw missing!"); break; } 
+        if (!isset ($_POST['smtp_pw'])) { printKO("smtp_pw missing!"); break; }
         $DATA['smtp_pw'] = $_POST['smtp_pw'];
         $DATA['smtp_isPW'] = empty($_POST['smtp_pw'])?false:true;
-        if (!isset ($_POST['smtp_secure'])) { printKO("smtp_secure missing!"); break; } 
-        if (empty($_POST['smtp_secure'])) { printKO("smtp_secure empty!"); break; }  
+        if (!isset ($_POST['smtp_secure'])) { printKO("smtp_secure missing!"); break; }
+        if (empty($_POST['smtp_secure'])) { printKO("smtp_secure empty!"); break; }
         $DATA['smtp_auth'] = ($_POST['smtp_secure']=='none')?false:true;
         $DATA['smtp_secure'] = $_POST['smtp_secure'];
-        if (!isset ($_POST['smtp_email'])) { printKO("smtp_email missing!"); break; } 
-        if (empty($_POST['smtp_email'])) { printKO("smtp_email empty!"); break; }  
+        if (!isset ($_POST['smtp_email'])) { printKO("smtp_email missing!"); break; }
+        if (empty($_POST['smtp_email'])) { printKO("smtp_email empty!"); break; }
         $DATA['smtp_email'] = $_POST['smtp_email'];
     }
-	
+
 
 // -------------------------------------------------------------------
 
     /* Prequist */
-	if(!function_exists('mcrypt_encrypt')) {
-        printKO("mcrypt isn't loaded!"); break; 
+    if(!function_exists('mcrypt_encrypt')) {
+        printKO("mcrypt isn't loaded!"); break;
     }
-    printOK("Prequist OK!"); break; 
-	/* 0. add debug/production info */
-	if (($_SERVER['SERVER_ADDR']=='localhost' ) || $_SERVER["SERVER_ADDR"]=="127.0.0.1" || ($_SERVER["SERVER_ADDR"]=="::1")) {
+    printOK("Prequist OK!");
+
+    /* 0. add debug/production info */
+    if (($_SERVER['SERVER_ADDR']=='localhost' ) || $_SERVER["SERVER_ADDR"]=="127.0.0.1" || ($_SERVER["SERVER_ADDR"]=="::1")) {
         $DATA['debug'] = true;
         printOK("Debug Mode");
     } else {
         $DATA['debug'] = false;
         printOK("Production Mode");
     }
-	
-	/* 1. Generate configuration file */
-	$string = @json_encode($DATA);
-	$encryptionMethod = 'aes128';
-	$iv = @mcrypt_create_iv(16, MCRYPT_DEV_RANDOM);
-	if (!@file_put_contents ($confPath, $iv.openssl_encrypt ($string, $encryptionMethod, $secretHash, NULL, $iv))) {
-		printKO("Failed to write to '$confPath'"); break; 
-	} else {
+
+    /* 1. Generate configuration file */
+    $string = @json_encode($DATA);
+    $encryptionMethod = 'aes128';
+    $iv = @mcrypt_create_iv(16, MCRYPT_DEV_RANDOM);
+    if (!@file_put_contents ($confPath, $iv.openssl_encrypt ($string, $encryptionMethod, $secretHash, NULL, $iv))) {
+        printKO("Failed to write to '$confPath'"); break;
+    } else {
         printOK("Write to '$confPath'");
         // bonus to allow read back
         @file_put_contents($hashPath, $secretHash);
     }
-    
+
     /* 2. Read back for test */
-	$encryptedMessage = @file_get_contents ($confPath);	
-	if (!$encryptedMessage) {
-		printKO("Failed to read back '$confPath'"); break; 		
-	} else {
+    $encryptedMessage = @file_get_contents ($confPath);
+    if (!$encryptedMessage) {
+        printKO("Failed to read back '$confPath'"); break;
+    } else {
         printOK("Read back '$confPath'");
     }
-	$iv = @substr($encryptedMessage, 0, 16);
-	$encryptedMessage = @substr($encryptedMessage, 16);
-	$readBack = @openssl_decrypt($encryptedMessage, $encryptionMethod, $secretHash, 0, $iv);
-	$DATA_BACK = @json_decode($readBack, true);	
+    $iv = @substr($encryptedMessage, 0, 16);
+    $encryptedMessage = @substr($encryptedMessage, 16);
+    $readBack = @openssl_decrypt($encryptedMessage, $encryptionMethod, $secretHash, 0, $iv);
+    $DATA_BACK = @json_decode($readBack, true);
     $foundError = false;
-	foreach ($DATA as $key => $value) {
-		if($DATA[$key]!=$DATA_BACK[$key]) {
-			printKO("$key: $value");
+    foreach ($DATA as $key => $value) {
+        if($DATA[$key]!=$DATA_BACK[$key]) {
+            printKO("$key: $value");
             $foundError = true;
-		} else {
+        } else {
             //printOK("$key: $value");
         }
-	}
+    }
     if ($foundError) break;
-    
+
     /* 3. Write conf.php file accordingly */
     $confPhpFile = @realpath (@getcwd()."/..");
-    $confPhpFile .= DIRECTORY_SEPARATOR . 'conf.php';    
+    $confPhpFile .= DIRECTORY_SEPARATOR . 'conf.php';
     $file = fopen($confPhpFile, "w");
     $nl='';
     /* $nl="\n"; */
@@ -164,48 +165,48 @@ if(true){try{do
     fwrite($file, '<?PHP {'.$nl);
     fwrite($file, '$e=@file_get_contents(\''.$confPath.'\');'.$nl);
     fwrite($file, 'if(!$e){echo("error 1");die();}'.$nl);
-    fwrite($file, '$i=@substr($e,0,16);'.$nl);       
-    fwrite($file, '$e=@substr($e,16);'.$nl);       
-    fwrite($file, '$m="aes128";'.$nl);           
-    fwrite($file, '$s=@openssl_decrypt($e,$m,\''.$secretHash.'\',0,$i);'.$nl);               
-    fwrite($file, 'if(!isset($s)||trim($s)===\'\'){echo("error 2");die();}'.$nl);                       
-    fwrite($file, '$GLOBALS[\'CONFIG\']=@json_decode($s,true);'.$nl);                           
-    fwrite($file, '$GLOBALS[\'CONFIG\'][\'root_path\']=realpath(dirname(__FILE__));'.$nl);                           
+    fwrite($file, '$i=@substr($e,0,16);'.$nl);
+    fwrite($file, '$e=@substr($e,16);'.$nl);
+    fwrite($file, '$m="aes128";'.$nl);
+    fwrite($file, '$s=@openssl_decrypt($e,$m,\''.$secretHash.'\',0,$i);'.$nl);
+    fwrite($file, 'if(!isset($s)||trim($s)===\'\'){echo("error 2");die();}'.$nl);
+    fwrite($file, '$GLOBALS[\'CONFIG\']=@json_decode($s,true);'.$nl);
+    fwrite($file, '$GLOBALS[\'CONFIG\'][\'root_path\']=realpath(dirname(__FILE__));'.$nl);
     fwrite($file, '} ?>'.$nl);
     /*fwrite($file, '<?PHP foreach ($GLOBALS[\'CONFIG\'] as $key => $value) { echo ("$key: $value<br>"); } ?>'); */
     fclose($file);
     printOK("write to '$confPhpFile'</br>Configuration Done!");
-    
-	/* 4. let's include conf.php */
-	if (!file_exists ('../conf.php')) { printKO("conf.php not correctly generated"); break; }
+
+    /* 4. let's include conf.php */
+    if (!file_exists ('../conf.php')) { printKO("conf.php not correctly generated"); break; }
     include('../conf.php');
-	printOK("inclusion of conf.php OK");
-    
+    printOK("inclusion of conf.php OK");
+
     /* 5. test mysql */
     if ($setup['features']['db']) {
         if ($GLOBALS['CONFIG']['sql_isPW']) {
-            $db = @mysql_connect($GLOBALS['CONFIG']['sql_host'], $GLOBALS['CONFIG']['sql_login'], $GLOBALS['CONFIG']['sql_pw']); 
+            $db = @mysql_connect($GLOBALS['CONFIG']['sql_host'], $GLOBALS['CONFIG']['sql_login'], $GLOBALS['CONFIG']['sql_pw']);
         } else {
-            $db = @mysql_connect($GLOBALS['CONFIG']['sql_host'], $GLOBALS['CONFIG']['sql_login']); 
+            $db = @mysql_connect($GLOBALS['CONFIG']['sql_host'], $GLOBALS['CONFIG']['sql_login']);
         }
         if ($db) {
             printOK('Connection to \''.$GLOBALS['CONFIG']['sql_host'].'\'');
         } else {
             printKO('Connection to \''.$GLOBALS['CONFIG']['sql_host'].'\''); break;
-        }    
+        }
         if(@mysql_select_db($GLOBALS['CONFIG']['sql_db'],$db)) {
             printOK('Connection to database \''.$GLOBALS['CONFIG']['sql_db'].'\'');
         } else {
             printKO('Connection to database \''.$GLOBALS['CONFIG']['sql_db'].'\'');
             break;
-        }    
+        }
 
         /* 6. apply setup.sql */
         $ret = applySqlFile($setup_sql);
         if ($ret) { printKO($ret); break; }
         printOK("Apply 'setup.sql'<br>Database Configured!");
     }
-    
+
     /* 7. add user table */
     if ($setup['features']['user']) {
         $replace_arr=array('%USER_TABLE_NAME%' => $GLOBALS['CONFIG']['user_table']);
@@ -213,32 +214,32 @@ if(true){try{do
         if ($ret) { printKO($ret); break; }
         printOK("Apply 'users.sql'<br>Users table Added!");
     }
-    
+
     /* 8. insert admin */
     if ($setup['features']['admin']) {
         $req =  "INSERT INTO `".$GLOBALS['CONFIG']['user_table']."` ";
-        $req .= "(`UUID`, `EMAIL`, `PASSWORD`, `PRIVILEGE`, `CREATION_DATE`) VALUES (";    
+        $req .= "(`UUID`, `EMAIL`, `PASSWORD`, `PRIVILEGE`, `CREATION_DATE`) VALUES (";
         $req .= "'".$DATA['admin_uuid']."',";
         $req .= "'".$DATA['admin_email']."',";
         $req .= "'".md5($DATA['admin_pw'])."',";
         $req .= "1,";
         $req .= "now())";
         if (!@mysql_query($req)){
-            printKO("Failed to add admin user<br>".mysql_error()); break; 		
+            printKO("Failed to add admin user<br>".mysql_error()); break;
         } else {
             printOK("Add admin user");
-        }    
+        }
     }
-    
+
     if ($setup['features']['db']) @mysql_close($db);
-        
+
     /* 8. test email */
     if ($setup['features']['mail']) {
         require '../libs/PHPMailer/PHPMailerAutoload.php';
         $mail = new PHPMailer();
         $mail->IsSMTP();
         $mail->SMTPDebug  = 4;
-        $mail->Host       = $GLOBALS['CONFIG']['smtp_host'];	
+        $mail->Host       = $GLOBALS['CONFIG']['smtp_host'];
         $mail->Port       = $GLOBALS['CONFIG']['smtp_port'];
         $mail->Username   = $GLOBALS['CONFIG']['smtp_login'];
         $mail->Password   = $GLOBALS['CONFIG']['smtp_pw'];
@@ -249,15 +250,15 @@ if(true){try{do
         $mail->addReplyTo($GLOBALS['CONFIG']['smtp_email']);
         $mail->addAddress($GLOBALS['CONFIG']['smtp_email']);
         $mail->Subject = "Test from ".$setup['title'];
-        $mail->Body = "<h1>If you can read this that mean your setup is correct!</h1>"; 
+        $mail->Body = "<h1>If you can read this that mean your setup is correct!</h1>";
         print("<tr><td><textarea style='width:100%;'>");
-        
+
         if($mail->Send()) {
             print("</textarea><br>");
             print("Send an email<br>&nbsp&nbspfrom: ".$GLOBALS['CONFIG']['smtp_email']."<br>&nbsp&nbspto:&nbsp&nbsp ".$DATA['smtp_email']);
             print("</td>");
             print("<td class='success'>OK</td>");
-        } else {   
+        } else {
             print("</textarea><br>");
             print("Failed to send an email<br>&nbsp&nbspfrom: ".$GLOBALS['CONFIG']['smtp_email']."<br>&nbsp&nbspto:&nbsp&nbsp ".$DATA['smtp_email']." ".$mail->ErrorInfo);
             print("</td>");
@@ -266,15 +267,15 @@ if(true){try{do
         }
     }
     $SUCESS_SETUP = true;
-    
+
 } while(0);}catch(Exception $e){echo'Exception: ',$e->getMessage(), "\n";}} else {
-    
+
     $SUCESS_SETUP = true;
     $DATA['base_url'] = "http://google.com";
     printOK("un");
     printOK("deux");
     printOK("trois");
-    
+
 }
 echo "</table>\n";
 
